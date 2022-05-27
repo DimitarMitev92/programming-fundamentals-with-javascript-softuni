0f1e09e2a4b9f2cd9005316c7991d64f39484444
Dimitar Mitev <d.mitev920123@gmail.com>
2022-05-27 11:36:23 +0300

    Create 03.rightPlace.js

---
+++ 04.Data Types and Variables - Lab/03. Right Place/03.rightPlace.js
@@ -0,0 +1,8 @@
+function solve(firstString, char, secondString) {
+    let result = firstString.replace('_', char);
+    if (result === secondString) {
+        console.log('Matched');
+    } else {
+        console.log(`Not Matched`);
+    }
+}

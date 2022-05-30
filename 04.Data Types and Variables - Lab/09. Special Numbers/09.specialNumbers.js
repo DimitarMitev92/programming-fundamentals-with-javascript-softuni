b09b156e256e534248bc15a39c6bbc98527488d9
Dimitar Mitev <d.mitev920123@gmail.com>
2022-05-30 09:12:18 +0300

    Create 09.specialNumbers.js

---
+++ 04.Data Types and Variables - Lab/09. Special Numbers/09.specialNumbers.js
@@ -0,0 +1,16 @@
+function solve(num) {
+    for (let i = 1; i <= num; i++) {
+        let sum = 0;
+        let stringNum = i.toString();
+        for (let j = 0; j < stringNum.length; j++) {
+            sum += Number(stringNum[j]);
+        }
+        let isSpecial;
+        if (sum === 5 || sum === 7 || sum === 11) {
+            isSpecial = 'True';
+        } else {
+            isSpecial = 'False';
+        }
+        console.log(`${i} -> ${isSpecial}`)
+    }
+}

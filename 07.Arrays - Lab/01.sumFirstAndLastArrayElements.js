67235bcbddb7c640366b32d7091742e638663620
Dimitar Mitev <d.mitev920123@gmail.com>
2022-06-08 08:48:27 +0300

    Create 01.sumFirstAndLastArrayElements.js

---
+++ 07.Arrays - Lab/01.sumFirstAndLastArrayElements.js
@@ -0,0 +1,6 @@
+function solve(arr) {
+    let firstElement = Number(arr[0]);
+    let lastElement = Number(arr[arr.length - 1]);
+    let result = firstElement + lastElement;
+    console.log(result)
+}

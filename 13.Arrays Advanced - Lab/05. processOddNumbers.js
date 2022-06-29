57ed4b96560839008d694bc534e5eae0b70ae89f
Dimitar Mitev <d.mitev920123@gmail.com>
2022-06-29 09:07:55 +0300

    Create 05. processOddNumbers.js

---
+++ 13.Arrays Advanced - Lab/05. processOddNumbers.js
@@ -0,0 +1,9 @@
+function processOddNumbers(arr) {
+    let result = (arr
+        .filter((num, i) => i % 2 === 1))
+        .map(x => x * 2)
+        .reverse()
+        .join(' ');
+    console.log(result);
+
+}

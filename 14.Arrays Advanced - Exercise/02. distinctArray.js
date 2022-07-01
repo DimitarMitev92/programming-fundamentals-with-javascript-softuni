e3946275c19f653de7de25a281f30186894b819f
Dimitar Mitev <d.mitev920123@gmail.com>
2022-07-01 08:23:46 +0300

    Create 02. distinctArray.js

---
+++ 14.Arrays Advanced - Exercise/02. distinctArray.js
@@ -0,0 +1,10 @@
+function distinctArray(arr) {
+    let newArr = [arr[0]];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (!newArr.includes(arr[i])) {
+            newArr.push(arr[i]);
+        }
+    }
+    console.log(newArr.join(' '));
+}
